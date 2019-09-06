f393022fb48babb3d0b79e3613637fc6d7762843
Tyler Gilchrist <tyler668@gmail.com>
2019-09-06 13:02:15 -0400

    Adding testing dependencies

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render, fireEvent} from '@testing-library/jest-dom';
+import PlayerCards  from './components/PlayerCards';
+import * as rtl from '@testing-library/react';
+import 'jest-dom/extend-expect';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders player cards correctly', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<PlayerCards />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+test('basic math', () =>{
+  expect(1+1).toBe(2);
+})
+
+// test('dark mode adds the dark mode class', () => {
+
+// })
