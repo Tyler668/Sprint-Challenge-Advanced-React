04e77526e9be5a7b6f3db0a63982509dbce04d35
Tyler Gilchrist <tyler668@gmail.com>
2019-09-06 11:44:26 -0400

    Player cards appearing in basic form

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,39 @@
 import React, { useState, Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import axios from 'axios';
+import './App.css';
 
 class App extends Component {
   // const [title, setTitle] = useState('Hello WEB 22!!!');
   constructor() {
     super();
     this.state = {
-      title: 'Hello WEB 22!!!',
-      count: 0
+      players: [
+
+      ]
     };
   }
 
 
+  componentDidMount() {
+    axios.get(`http://localhost:5000/api/players`)
+      .then(res => {
+        console.log(res.data);
+        this.setState({ players: res.data });
+      })
+      .catch(err => { console.log('Oof') })
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Test</h1>
-        <h2>Test2</h2>
+        {this.state.players.map(player => (
+          <div className = 'player-card'>
+            <h2>{player.name}</h2>
+            <h3>{player.country}</h3>
+            <h4>{player.searches} searches</h4>
+          </div>
+        ))}
       </div>
     );
   }
