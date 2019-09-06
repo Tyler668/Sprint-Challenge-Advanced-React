1db9034fe77e2a2310425260def3c089b146cb20
Tyler Gilchrist <tyler668@gmail.com>
2019-09-06 11:22:40 -0400

    Dependencies added and class structure set

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, Component } from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  // const [title, setTitle] = useState('Hello WEB 22!!!');
+  constructor() {
+    super();
+    this.state = {
+      title: 'Hello WEB 22!!!',
+      count: 0
+    };
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Test</h1>
+        <h2>Test2</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
